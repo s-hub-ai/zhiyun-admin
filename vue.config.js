dc76cd8cdc43f91eefa49b00f59070070d020d3f
zhangyan <407524148@qq.com>
2020-12-03 21:44:15 +0800

    。。

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ function resolve(dir) {
 }
 
 module.exports = {
-	publicPath: "/admin/",
+	publicPath: "/",
 	lintOnSave: true,
 	productionSourceMap: false,
 	parallel: require("os").cpus().length > 1,
